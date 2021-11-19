e7653f19a0e098dfc1e42030be175f53e974a124
Billy K. Bett <82142770+bkb-Git@users.noreply.github.com>
2021-11-19 15:54:58 +0300

    added links to the navbar

--- Components/Navbar/Navbar.jsx
+++ Components/Navbar/Navbar.jsx
@@ -1,14 +1,27 @@
 import { Menu } from "antd";
+import { useRouter } from "next/dist/client/router";
 
 import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
+  const router = useRouter();
+
+  const handleRoute = (e) => router.push(`${e.key}`);
+
   return (
-    <Menu mode="horizontal" className={styles.navbar}>
-      <Menu.Item key="Home">Home</Menu.Item>
-      <Menu.Item key="Plans">Hotels</Menu.Item>
-      <Menu.Item key="Services">Services</Menu.Item>
-      <Menu.Item key="About us">About us</Menu.Item>
+    <Menu selectedKeys={router.asPath} mode="horizontal" className={styles.navbar}>
+      <Menu.Item key="/" onClick={handleRoute}>
+        Home
+      </Menu.Item>
+      <Menu.Item key="/hotels" onClick={handleRoute}>
+        Hotels
+      </Menu.Item>
+      <Menu.Item key="/services" onClick={handleRoute}>
+        Services
+      </Menu.Item>
+      <Menu.Item key="/about" onClick={handleRoute}>
+        About us
+      </Menu.Item>
     </Menu>
   );
 };
---
+++ pages/hotels/hotels.jsx
@@ -0,0 +1,5 @@
+const hotels = () => {
+  return <div>Hotel Page Here!</div>;
+};
+
+export default hotels;
